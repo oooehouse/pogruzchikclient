0536604bb3617fd7e89373a6a20b844c08fd7769
Yauhen Hloba <globaevg@gmail.com>
2019-09-09 23:56:27 +0300

    Updates

---
+++ precache-manifest.a96cd3bf0ff697b860ef5b53e91b272c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb541a2a96a642f62b15cfb4623c8a2d",
+    "url": "/pogruzchikclient/index.html"
+  },
+  {
+    "revision": "1dab8e5dd92d09876bfa",
+    "url": "/pogruzchikclient/static/css/main.0321abcd.chunk.css"
+  },
+  {
+    "revision": "c6f599324e4631fc23c5",
+    "url": "/pogruzchikclient/static/js/2.c918b11a.chunk.js"
+  },
+  {
+    "revision": "1dab8e5dd92d09876bfa",
+    "url": "/pogruzchikclient/static/js/main.5ba2732d.chunk.js"
+  },
+  {
+    "revision": "b7036450eb5c364bf041",
+    "url": "/pogruzchikclient/static/js/runtime~main.e012db55.js"
+  },
+  {
+    "revision": "de6e8c68af70aa93fa6095e92af861b1",
+    "url": "/pogruzchikclient/static/media/case_concept.de6e8c68.jpg"
+  },
+  {
+    "revision": "1406d34370ea4dc5c3151eccb7df73fc",
+    "url": "/pogruzchikclient/static/media/cat-el-pogr.1406d343.jpg"
+  },
+  {
+    "revision": "c0cdf79c8fc1e6d7271b605ee39ab739",
+    "url": "/pogruzchikclient/static/media/doosan.c0cdf79c.jpg"
+  },
+  {
+    "revision": "c01556f73615a2eb52f69c052b26020b",
+    "url": "/pogruzchikclient/static/media/front_pogr.c01556f7.png"
+  },
+  {
+    "revision": "4f6f88c4096daefd248f1326610b3eab",
+    "url": "/pogruzchikclient/static/media/mini_pogr.4f6f88c4.jpg"
+  },
+  {
+    "revision": "146afef26e0f770ba4778f3efe7983d8",
+    "url": "/pogruzchikclient/static/media/pogr_building.146afef2.jpg"
+  },
+  {
+    "revision": "3ab07ba052ced197031f67328d0d15de",
+    "url": "/pogruzchikclient/static/media/pogr_right.3ab07ba0.jpg"
+  },
+  {
+    "revision": "634f7162fee3648b41e42356708b574e",
+    "url": "/pogruzchikclient/static/media/porg_left.634f7162.jpg"
+  },
+  {
+    "revision": "af7febfd84f9578bf9e2722a32b1815c",
+    "url": "/pogruzchikclient/static/media/telescop_pogr.af7febfd.jpg"
+  },
+  {
+    "revision": "5ca44417507e014f7af9a4409ef50338",
+    "url": "/pogruzchikclient/static/media/univers_front_pogr.5ca44417.jpg"
+  }
+]);
--- src/redux/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import news from './news/reducer.js';
-const rootReducer = combineReducers({
-  news
-});
-
-export default rootReducer;
